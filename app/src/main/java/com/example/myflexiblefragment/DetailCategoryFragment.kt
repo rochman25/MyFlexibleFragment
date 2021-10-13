0d298fff7b3e197a64e48689e4eb487b60acf430
rochman25 <zaenur.rochman98@gmail.com>
2021-10-13 14:53:25 +0700

    add some fragment
    - add detailcategoryfragment
    - add dialogfragment
    - and add new activity

--- app/src/main/java/com/example/myflexiblefragment/DetailCategoryFragment.kt
+++ app/src/main/java/com/example/myflexiblefragment/DetailCategoryFragment.kt
@@ -1,5 +1,6 @@
 package com.example.myflexiblefragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 /**
  * A simple [Fragment] subclass.
@@ -51,5 +53,25 @@ class DetailCategoryFragment : Fragment() {
             tvCategoryName.text = categoryName
             tvCategoryDescription.text = description
         }
+        btnShowDialog.setOnClickListener{
+            val mOptionDialogFragment = OptionDialogFragment()
+
+            val mFragmentManager = childFragmentManager
+            mOptionDialogFragment.show(mFragmentManager, OptionDialogFragment::class.java.simpleName)
+        }
+
+        btnProfile.setOnClickListener {
+            val mIntent = Intent(activity, ProfileActivity::class.java)
+            startActivity(mIntent)
+        }
     }
+
+    internal var optionDialogListener: OptionDialogFragment
+    .OnOptionDialogListener = object : OptionDialogFragment.OnOptionDialogListener {
+        override fun onOptionChosen(text: String?) {
+            Toast.makeText(activity, text, Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
 }
